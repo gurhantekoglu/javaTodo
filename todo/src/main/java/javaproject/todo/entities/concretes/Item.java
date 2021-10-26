81bac37b67ec442ae8d5c2dfd7c97ceb709353c4
Gürhan TEKOĞLU <gurhantekoglu@gmail.com>
2021-10-27 01:45:27 +0300

    Create project - Add User and Item in entities

---
+++ todo/src/main/java/javaproject/todo/entities/concretes/Item.java
@@ -0,0 +1,49 @@
+package javaproject.todo.entities.concretes;
+
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@Inheritance(strategy = InheritanceType.JOINED)
+@Table(name = "items")
+@NoArgsConstructor
+@AllArgsConstructor
+public class Item {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private int id;
+
+	@JoinColumn(name = "user_id")
+	@ManyToOne()
+	private int userId;
+
+	@Column(name = "name")
+	private String name;
+
+	@Column(name = "description")
+	private String description;
+
+	@JsonIgnore
+	@Column(name = "created_date", columnDefinition = "Date default CURRENT_DATE")
+	private LocalDate createdDate = LocalDate.now();
+
+}
