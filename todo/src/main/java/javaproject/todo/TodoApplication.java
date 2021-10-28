f3755565e4272f5a4e458e47b8c9624b16194388
Gürhan TEKOĞLU <gurhantekoglu@gmail.com>
2021-10-28 22:38:25 +0300

    Add swagger

--- todo/src/main/java/javaproject/todo/TodoApplication.java
+++ todo/src/main/java/javaproject/todo/TodoApplication.java
@@ -3,11 +3,22 @@ package javaproject.todo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
+@EnableSwagger2
 public class TodoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TodoApplication.class, args);
+	}
+
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(RequestHandlerSelectors.basePackage("javaproject.todo")).build();
 	}
 
 }
